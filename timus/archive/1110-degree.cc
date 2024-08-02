ad03c5e786bd6f0e96d39f6c88b11666a5b0a145
Misha Kobzar <knamgie@yandex.ru>
2024-08-02 13:11:10 +0500

    August 2, 2024 at 13:11

---
+++ timus/archive/1110-degree.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+using namespace std;
+
+void Solve() {
+  i32 n, m, y;
+  cin >> n >> m >> y;
+
+  vector<i32> xs;
+  for (i32 x = 0; x < m; x++) {
+    i32 xn = 1;
+    for (i32 i = 0; i < n; i++) {
+      xn = (xn * x) % m;
+    }
+    if (xn == y) {
+      xs.push_back(x);
+    }
+  }
+
+  if (xs.empty()) {
+    cout << -1;
+  } else {
+    for (i32 x : xs) {
+      cout << x << ' ';
+    }
+  }
+}
+
+int main() {
+  Solve();
+}
