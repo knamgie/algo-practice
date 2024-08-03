1d7d917c544e8b0f46be1044819a97b6c777c00f
Misha Kobzar <knamgie@yandex.ru>
2024-08-03 11:12:07 +0500

    August 3, 2024 at 11:12

---
+++ codeforces/problemset/main/1995a-diagonals.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+using namespace std;
+
+void Solve() {
+  i32 n, k;
+  cin >> n >> k;
+
+  i32 ans = 0;
+  bool flag = false;
+  while (k > 0) {
+    ans++;
+    k -= n;
+    flag = !flag;
+    if (flag) {
+      n--;
+    }
+  }
+  cout << ans << '\n';
+}
+
+int main() {
+  i32 t;
+  cin >> t;
+  while (t--) {
+    Solve();
+  }
+}
