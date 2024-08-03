efa9c9b512ce2bfc613492ad96680498a3176fe0
Misha Kobzar <knamgie@yandex.ru>
2024-08-03 22:41:44 +0500

    August 3, 2024 at 22:41

---
+++ codeforces/problemset/main/1146b-hate-a.cc
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+using namespace std;
+
+void Solve() {
+  string t;
+  cin >> t;
+
+  string s, ss;
+  i32 j = 0;
+  while (s.size() + ss.size() < t.size()) {
+    s += t[j];
+    if (t[j] != 'a') {
+      ss += t[j];
+    }
+    j++;
+  }
+
+  if (ss == t.erase(0, j)) {
+    cout << s;
+  } else {
+    cout << ":(";
+  }
+}
+
+int main() {
+  Solve();
+}
