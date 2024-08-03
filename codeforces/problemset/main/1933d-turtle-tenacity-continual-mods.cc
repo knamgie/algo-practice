bf2c64b3f7a82f32e27d5ba1ba3c0795f7395dd2
Misha Kobzar <knamgie@yandex.ru>
2024-08-03 16:34:11 +0500

    August 3, 2024 at 16:34

---
+++ codeforces/problemset/main/1933d-turtle-tenacity-continual-mods.cc
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+using namespace std;
+
+void Solve() {
+  i32 n;
+  cin >> n;
+  vector<i32> a(n);
+  for (i32& ai : a) {
+    cin >> ai;
+  }
+
+  sort(a.begin(), a.end());
+  if (a[0] != a[1]) {
+    cout << "YES\n";
+    return;
+  }
+
+  for (i32 ai : a) {
+    if (ai % a[0] != 0) {
+      cout << "YES\n";
+      return;
+    }
+  }
+  cout << "NO\n";
+}
+
+int main() {
+  i32 t;
+  cin >> t;
+  while (t--) {
+    Solve();
+  }
+}
