8431c3ca89a381fbeb27135e6ee078f7b30cbcd7
Misha Kobzar <knamgie@yandex.ru>
2024-08-04 21:27:55 +0500

    August 4, 2024 at 21:27

---
+++ timus/archive/1688-team-gov.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using i32 = int32_t;
+using i64 = int64_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using f32 = float;
+using f64 = double;
+
+using namespace std;
+
+void Solve() {
+  i64 n;
+  i32 m;
+  cin >> n >> m;
+  vector<i64> paids(m);
+  for (i64& paid : paids) {
+    cin >> paid;
+  }
+
+  n *= 100;
+  for (i64& paid : paids) {
+    paid *= 100;
+  }
+
+  i64 v = 0;
+  i32 i = 0;
+  while (i < paids.size() && v < n) {
+    v += paids[i] / 3;
+    i++;
+  }
+
+  if (v >= n) {
+    cout << "Free after " << i << " times.";
+  } else {
+    cout << "Team.GOV!";
+  }
+}
+
+int main() {
+  Solve();
+}
